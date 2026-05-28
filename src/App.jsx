c7a2125239fafe17e5d6aa3a4f8527a24de2f244
sshaheen3 <shaikanjuman_shaheen@apple.com>
2026-05-28 17:34:56 +0530

    Add Todo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,13 @@
-import { useState } from "react";
+import Todo from "./Todo";
+import { ComponentSharing } from "./ComponentSharing";
 import "./App.css";
 
 function App() {
-  const [items, setItems] = useState([
-    { id: 1, label: "study react", done: false },
-    { id: 2, label: "study node", done: false },
-  ]);
-
-  const handleDelete = (id) => {
-    const filtered = items.filter((item) => item.id !== id);
-    setItems(filtered);
-  };
-
-  const toggleDone = (id) => {
-    const toggle = items.map((item) => {
-      if (item.id == id)
-        return {
-          ...item,
-          done: !item.done,
-        };
-
-      return item;
-    });
-
-    setItems(toggle);
-  };
-
-  const handleAdd = () => {
-    setItems([
-      ...items,
-      {
-        id: Date.now(),
-        label: "Sample Todo",
-        done: false,
-      },
-    ]);
-  };
-
   return (
-    <>
-      <section id="center">
-        <ul>
-          {items.map((item) => {
-            return (
-              <li key={item.id}>
-                <p
-                  style={{
-                    textDecoration: item.done ? "line-through" : "none",
-                  }}
-                >
-                  {item.label}
-                </p>
-
-                <button onClick={() => toggleDone(item.id)}>
-                  {item.done ? "Undo" : "Done"}
-                </button>
-
-                <button onClick={() => handleDelete(item.id)}>Delete</button>
-              </li>
-            );
-          })}
-          <button onClick={handleAdd}>Add Todo</button>
-        </ul>
-      </section>
-    </>
+    <div id="center">
+      <ComponentSharing />
+      {/* <Todo /> */}
+    </div>
   );
 }
 
