e602cc955eab5c81226f2f86a2fa9a858796c37e
sshaheen3 <shaikanjuman_shaheen@apple.com>
2026-06-02 17:39:44 +0530

    Enhance SimpleCounter with interval functionality and reset history; update styles for better UI

--- src/SimpleCounter.jsx
+++ src/SimpleCounter.jsx
@@ -6,6 +6,7 @@ export const SimpleCounter = () => {
   const [counter, setCounter] = useState(0);
   const [stepValue, setStepValue] = useState(1);
   const [history, setHistory] = useState([]);
+  const [intervalId, setIntervalId] = useState(null);
 
   const maxCounter = 20;
   const evenOdd = counter % 2 === 0 ? "Even" : "Odd";
@@ -33,6 +34,7 @@ export const SimpleCounter = () => {
   const handleReset = () => {
     setCounter(0);
     setStepValue(1);
+    setHistory([]);
   };
 
   const onChange = (e) => setStepValue(Number(e.target.value));
@@ -45,24 +47,65 @@ export const SimpleCounter = () => {
     setCounter(newhistory.at(-1));
   };
 
+  const handleStartInterval = () => {
+    if (intervalId) return;
+    console.log("Interval Running");
+
+    let id = setInterval(() => {
+      setCounter((prev) => {
+        const newCount = Math.min(prev + 10, maxCounter);
+
+        if (newCount === maxCounter) {
+          handleStopInterval(id);
+          //   clearInterval(id);
+          //   setIntervalId(null);
+          //   console.log("Stopped");
+        }
+
+        return newCount;
+      });
+    }, 1000);
+    console.log(id);
+    setIntervalId(id);
+  };
+
+  const handleStopInterval = (intervalId) => {
+    if (!intervalId) return;
+    clearInterval(intervalId);
+    setIntervalId(null);
+    console.log("Stopped");
+  };
+
   return (
     <>
       <p>
         Counter: {counter} - {evenOdd}
       </p>
-      {maxCounter === counter && <p>"Max limit reached"</p>}
+
       <p>History: {history.join()}</p>
-      <button onClick={() => handleIncrement()}>Increment</button>
-      <button onClick={() => handleDecrement()} disabled={disableDecrement}>
-        Decrement
-      </button>
-      <button onClick={handleUndo}>Undo</button>
+      <div className="counter">
+        <button onClick={() => handleIncrement()}>Increment</button>
+        <button onClick={() => handleDecrement()} disabled={disableDecrement}>
+          Decrement
+        </button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleStartInterval}>Start Interval</button>
+        <button onClick={() => handleStopInterval(intervalId)}>
+          Stop Interval
+        </button>
+      </div>
+
       <button onClick={handleReset}>Reset</button>
       <p>
         Step Value:
         <input type="number" value={stepValue} onChange={(e) => onChange(e)} />
+        {stepValue}
       </p>
-      <p>Current Step: {stepValue}</p>
+      {/* <p>Current Step: </p> */}
+
+      {maxCounter === counter && (
+        <p className="success">Counter max limit reached i.e {maxCounter} </p>
+      )}
     </>
   );
 };
