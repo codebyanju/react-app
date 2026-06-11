8e7f5fd4cdabd1fe5d33e7136c23c3bfa4ac879b
anju <codebyanju@gmail.com>
2026-06-11 20:22:56 +0530

    custom hook useeffect

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -10,20 +10,26 @@ const useRestaurantMenu = (resId) => {
     fetchRestaurantMenu();
   }, [resId]);
 
+  // Swiggy API Not working
+  //   const fetchRestaurantMenu = async () => {
+  //     const url =
+  //       "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.3623546&lng=76.97870759999999&restaurantId=" +
+  //       resId;
+  //     const data = await fetch(url);
+  //     console.log("data", data);
+
+  //     const json = await data.json();
+  //     console.log("json", json);
+
+  //     setResMenu(json.data.cards[2].card.card.info);
+  //   };
+
   const fetchRestaurantMenu = async () => {
-    const data =
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.3623546&lng=76.97870759999999&restaurantId=" +
-      resId; // Swiggy API Not working
-
-    console.log("data", data);
-    // console.log("data.json()", json);
-    // setResMenu(json.data.cards[2].card.card.info);
-
-    // const data = await fetch(
-    //   "https://namastedev.com/api/v1/listRestaurantMenu/123456",
-    // );
-    // const json = await data.json();
-    // setResMenu(json.data.cards[2].card.card.info);
+    const data = await fetch(
+      "https://namastedev.com/api/v1/listRestaurantMenu/123456",
+    );
+    const json = await data.json();
+    setResMenu(json.data.cards[2].card.card.info);
   };
 
   return {
